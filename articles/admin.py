5d72ccf17a272bb31fcc97dc01b673b36ebc4c54
macurrent <bruceminanga@gmail.com>
2021-06-20 07:49:32 +0300

    comment field created

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
-admin.site.register(Article)
+
+class CommentInline(admin.TabularInline):  # new
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):  # new
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)  # new
+admin.site.register(Comment)
