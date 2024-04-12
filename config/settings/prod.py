4610e26d8c14fc3f9afcd3711295fb8be53ddf42
bruceminanga <bruceminanga@gmail.com>
2024-04-12 23:47:31 +0300

    databases

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -31,16 +31,10 @@ ADMINS = [
 
 ALLOWED_HOSTS = ['newspaperapp.nurseprofessors.com']
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
   'default': {
-      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+      'ENGINE': 'django.db.backends.postgresql',
       'NAME': os.getenv('DB_NAME'),
       'USER': os.getenv('DB_USER'),
       'PASSWORD': os.getenv('DB_PASSWORD'),
